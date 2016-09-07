c4d617be5a8dc595502860ff866e522412d7cdf2
Taylor Britton <k.taylor.britton@gmail.com>
2016-09-07 09:48:55 -0700

    what is going on?

--- js/routes.js
+++ js/routes.js
@@ -5,6 +5,7 @@ AppRoutes.$inject = ['$stateProvider', '$urlRouterProvider'];
 
 function AppRoutes($stateProvider, $urlRouterProvider) {
   $stateProvider
+    // This is the pages branch
     .state('home', {
       url: '/',
       templateUrl: './views/home.html',
