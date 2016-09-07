dafe6979cd7bf55015e2452469d2eb31e604d5b6
Taylor Britton <k.taylor.britton@gmail.com>
2016-09-07 09:42:26 -0700

    trying to push this stupid thing properly

--- js/routes.js
+++ js/routes.js
@@ -7,7 +7,7 @@ function AppRoutes($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: '../views/home.html',
+      templateUrl: './views/home.html',
       controller: 'LoginController',
       controllerAs: 'loginCtrl'
     });
