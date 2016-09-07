35c431fee28675df1f2fb0acc833e37cccd15c50
Taylor Britton <k.taylor.britton@gmail.com>
2016-09-07 09:37:05 -0700

    views from templates

--- js/routes.js
+++ js/routes.js
@@ -7,7 +7,7 @@ function AppRoutes($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('home', {
       url: '/',
-      templateUrl: './templates/home.html',
+      templateUrl: './views/home.html',
       controller: 'LoginController',
       controllerAs: 'loginCtrl'
     });
