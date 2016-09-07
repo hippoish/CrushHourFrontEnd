e94314ab87686d5becc74511d770b4c7d957498c
Taylor Britton <k.taylor.britton@gmail.com>
2016-09-07 09:11:10 -0700

    ngmaps showing user's current location

--- js/app.js
+++ js/app.js
@@ -1,25 +1,62 @@
 (function() {
 
   angular.module('CrushHourApp', ['satellizer', 'ui.router', 'ngMap'])
-    .config(function($authProvider) {
+    // .controller('MainController', MainController)
+    .config(AuthProvider);
 
-      $authProvider.facebook({
-        clientId: '1793210427603289',
-        name: 'facebook',
-        url: 'http://localhost:3000/auth/facebook',
-        authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
-        redirectUri: window.location.origin + '/',
-        requiredUrlParams: ['display', 'scope'],
-        scope: ['email'],
-        scopeDelimiter: ',',
-        display: 'popup',
-        oauthType: '2.0',
-        popupOptions: { width: 580, height: 400 }
-      });
-      //
-      // $authProvider.google({
-      //   clientId: 'crush-hour-1473133696259'
-      // });
+  // MainController.$inject = ['$auth', '$window', '$http'];
+  AuthProvider.$inject = ['$authProvider'];
 
-    })
+  // function MainController($auth, $window, $http) {
+  //   var self = this;
+  //
+  //   self.authenticate = function(provider) {
+  //     $auth.authenticate(provider);
+  //   };
+  //   self.isAuthenticated = function() {
+  //     return $auth.isAuthenticated();
+  //   };
+  //   self.logout = function() {
+  //     $auth.logout();
+  //     delete $window.localStorage.currentUser;
+  //   };
+  //
+  //   self.pullFacebook = function() {
+  //     return $http.get('http://crushhour.herokuapp.com/api/users')
+  //   }
+  // }
+
+  function AuthProvider($authProvider) {
+    $authProvider.httpInterceptor = function() { return true; },
+    $authProvider.withCredentials = false;
+    $authProvider.tokenRoot = null;
+    $authProvider.baseUrl = '/';
+    $authProvider.loginUrl = '/auth/login';
+    $authProvider.signupUrl = '/auth/signup';
+    $authProvider.unlinkUrl = '/auth/unlink/';
+    $authProvider.tokenName = 'token';
+    $authProvider.tokenPrefix = 'satellizer';
+    $authProvider.tokenHeader = 'Authorization';
+    $authProvider.tokenType = 'Bearer';
+    $authProvider.storageType = 'localStorage';
+
+    $authProvider.facebook({
+      clientId: '1793210427603289',
+      name: 'facebook',
+      url: 'https://crushhour.herokuapp.com/auth/facebook',
+      authorizationEndpoint: 'https://www.facebook.com/v2.5/dialog/oauth',
+      redirectUri: window.location.origin + '/',
+      requiredUrlParams: ['display', 'scope'],
+      scope: ['email'],
+      scopeDelimiter: ',',
+      display: 'popup',
+      oauthType: '2.0',
+      popupOptions: { width: 580, height: 400 }
+    });
+    //
+    // $authProvider.google({
+    //   clientId: 'crush-hour-1473133696259'
+    // });
+
+  }
 })();
--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -1,12 +1,23 @@
 angular.module('CrushHourApp')
   .controller('LoginController', LoginController);
 
-LoginController.$inject = ['$auth'];
+LoginController.$inject = ['$auth', '$window', '$http'];
 
-function LoginController($auth) {
+function LoginController($auth, $window, $http) {
   var self = this;
 
   self.authenticate = function(provider) {
     $auth.authenticate(provider);
   };
+  self.isAuthenticated = function() {
+    return $auth.isAuthenticated();
+  };
+  self.logout = function() {
+    $auth.logout();
+    delete $window.localStorage.currentUser;
+  };
+
+  self.pullFacebook = function() {
+    return $http.get('http://crushhour.herokuapp.com')
+  }
 }
